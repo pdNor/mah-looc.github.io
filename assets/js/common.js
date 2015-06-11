9499b3348b105438c84acecbd855dba5bc9448eb
Sebastian Bengtegård <sebbebook@gmail.com>
2015-06-11 11:12:11 +0200

    added quiz support, tweaks and misc stuff

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,6 +3,70 @@
     // Strict mode
     "use strict";
 
+    // Dependencies
+    var Http = window.qwest,
+        Storage = window.localStorage,
+        Cache;
+
+    var Endpoints = {
+        data: "/data.json",
+        submitQuiz: "http://0.0.0.0:3000/api/quiz"
+    };
+
+    // TODO: check if localStorage exists
+    // Setup cache
+    if (Storage.getItem("_mah-looc-data")) {
+        Cache = JSON.parse(Storage.getItem("_mah-looc-data"));
+    } else {
+        Cache = { user: null, domains: null, quizAnswers: null, lastUpdated: null };
+    }
+
+    function updateStorage() {
+        Storage.setItem("_mah-looc-data", JSON.stringify(Cache));
+    }
+
+    // Check wether the user is logged in or not
+    function userExists() {
+        if (Storage.getItem("_mah-looc-data")) {
+            var c = JSON.parse(Storage.getItem("_mah-looc-data"));
+
+            if (c.user) {
+                Cache.user = c.user;
+                updateStorage();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Fetches Quiz answers and stores them in the cache
+    function fetchQuizAnswers(cb) {
+        if (Cache.quizAnswers) {
+            return cb(Cache.quizAnswers);
+        }
+
+        return Http.get(Endpoints.data)
+            .then(function(res) {
+                Cache.quizAnswers = res.answers;
+                updateStorage();
+                cb(Cache.quizAnswers);
+            })
+            .catch(function(e, url) {
+                // TODO: better error handling
+                console.log(e, url);
+            });
+    }
+
+    // Initial fetch of quiz answers
+    fetchQuizAnswers(function(r) {
+        // DEBUG
+        console.log("Quiz Answers object:", r);
+    });
+
+    // DEBUG
+    if (Cache.user) console.log("User object:", Cache.user);
+
     // Convert a HTML NodeList to an array
     function toArray(nodes) {
         var arr = [],
@@ -71,6 +135,7 @@
 
         // Make sidebar fixed when scrolling
         var header = byId("header");
+
         window.addEventListener("scroll", function() {
             var top = document.scrollTop || document.body.scrollTop;
 
@@ -146,6 +211,7 @@
     function addAnchorsToHeaders() {
         var headers = bySelector("#content h1, #content h2, #content h3");
 
+        // Utilizes the id created from the header content by jekyll
         headers.filter(function(h) {
             return h.id ? true : false;
         }).forEach(function(h) {
@@ -264,6 +330,7 @@
         }
 
         var btn = createLineNumberButton();
+
         click(btn, function() {
             this.parentElement.classList.toggle("toggle");
         });
@@ -292,6 +359,7 @@
                     return prev + curr.textContent;
                 }, "");
 
+        // TODO: better parsing should be done
         data[language.substr(9)] = content;
         data["title"] = "Code Example";
         return data;
@@ -356,9 +424,238 @@
         return examples;
     }
 
+    function createFormAlternatives(alternativeList, index) {
+        // Determines wether we should create checkboxes or radiobuttons
+        var isMultiple = alternativeList.classList.contains("multiple");
+        var container = create("div");
+        var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "j"];
+
+        toArray(alternativeList.children).forEach(function(li, i) {
+            var label = create("label");
+            var input = create("input");
+            input.type = isMultiple ? "checkbox" : "radio";
+            input.name = "answer-" + index;
+            input.value = alpha[i];
+            
+            // TODO: should we worry about this?
+            // input.name = isMultiple ? "answer-" + index + "[]" : "answer-" + index;
+
+            label.appendChild(input);
+            label.innerHTML += li.innerHTML;
+            container.appendChild(label);
+        });
+
+        // TODO: shuffle alternatives?
+        return container;
+    }
+
+    // Quiz
+    function setupQuiz() {
+        var alternativeLists = bySelector("#quiz-container .alternatives");
+
+        // Don't do anything if there is no quiz/questions
+        if (!alternativeLists.length) {
+            return false;
+        }
+
+        // convert <ul> of alternatives to radio/checkbutton inputs
+        alternativeLists.forEach(function(alternativeList, index) {
+            var formAlternatives = createFormAlternatives(alternativeList, index);
+            alternativeList.parentNode.replaceChild(formAlternatives, alternativeList);
+        });
+
+        var quizForm = byId("quiz-form"),
+            quizMessage = byId("quiz-message");
+
+        quizForm.addEventListener("submit", function(e) {
+            e.preventDefault();
+
+            // Show message if no user is active
+            if (!userExists()) {
+                quizMessage.textContent = "Du måste vara inloggad för att kunna svara på denna Quiz.";
+                quizMessage.className = "info";
+                return false;
+            }
+
+            // Reset message
+            quizMessage.textContent = "";
+            quizMessage.className = "";
+
+            // Serialize the form data into an object
+            var formData = toArray(e.target)
+                    .filter(function(el) {
+                        // Only filter for input elements (radio + checkbox in this case)
+                        return (el.tagName == "INPUT") && el.type == "checkbox" || el.type == "radio";
+                    })
+                    .reduce(function(acc, curr, i, arr) {
+                        // Check if previous input is from a different group of alternatives.
+                        // So we can add the appropriate object attribute for the payload
+                        if (i == 0 || (i > 0 && arr[i].name !== arr[i - 1].name)) {
+                            // Checkboxes will be an array of answers and radiobuttons only a string
+                            acc[curr.name] = curr.type == "checkbox" ? [] : "";
+                        }
+
+                        // If isnt checked just return the accumulator
+                        if (!curr.checked) {
+                            return acc;
+                        }
+
+                        // Add the chosen answer(s)
+                        if (curr.type == "checkbox") {
+                            acc[curr.name].push(curr.value);
+                        } else {
+                            acc[curr.name] = curr.value;
+                        }
+
+                        return acc;
+                    }, {});
+
+            console.log(formData);
+
+            var quizId = byId("quiz-id").value,
+                moduleId = byId("module-id").value;
+
+            // Reduce to the current quiz
+            var currQuizAnswers = Cache.quizAnswers
+                    .filter(function(a) { return a.id == quizId; })[0];
+
+            // TODO: check if no quiz was found?
+
+            var results = currQuizAnswers.answers.map(function(answer, i) {
+                // TODO: check if no answers was found by user?
+                var userAnswer = formData["answer-" + i];
+
+                // Radio button
+                if (answer.type == "radio") {
+                    return answer.correct == userAnswer;
+                }
+
+                // Not enough correct checkboxes
+                if (userAnswer.length !== answer.correct.length) {
+                    return false;
+                }
+
+                // TODO: this doesnt really work with shuffled answers? perhaps if we sort them first?
+                return answer.correct.every(function(a, i) {
+                    return a == userAnswer[i];
+                });
+            });
+
+            // Amount of correct answers
+            var correctAnswers = results.reduce(function(p, c) { return c ? p + 1 : p + 0; }, 0);
+
+            // TODO: here we could use the quiz level required to pass
+
+            quizMessage.textContent = "Antal rätt svar: " + correctAnswers + "/" + currQuizAnswers.answers.length;
+            quizMessage.className = "info";
+
+            console.log(results);
+
+            var payload = {
+                id: Cache.user.id, 
+                code: Cache.user.code,
+                quiz: quizId,
+                module: moduleId,
+                hash: Cache.user.paths[0].hash,
+                answers: formData,
+                done: correctAnswers == currQuizAnswers.answers.length
+            };
+
+            return Http.post(Endpoints.submitQuiz, payload, { dataType: "json"})
+                .then(function(res) { return JSON.parse(res); })
+                .then(function(res) {
+                    Cache.user = res.user;
+                    updateStorage();
+
+                    if (correctAnswers == currQuizAnswers.answers.length) {
+                        var index,
+                            m;
+
+                        Cache.user.paths[0].modules.forEach(function(module, i, arr) {
+                            if (module.mid == moduleId && arr[i + 1]) {
+                                index = i + 1;
+                                m = arr[i + 1];
+                            }
+                        });
+
+                        var progressLink = create("a");
+                        progressLink.className = "submit-quiz-form";
+
+                        if (index) {
+                            progressLink.textContent = "Gå vidare";
+                            progressLink.href = "/domains/" + m.domain + "/modules/" + m.mid + ".html";
+                        } else {
+                            progressLink.textContent = "Profil";
+                            progressLink.href = "/path/profile.html";
+                        }
+
+                        var submitButton = byId("submit-quiz");
+                        submitButton.parentNode.replaceChild(progressLink, submitButton);
+                    } else {
+                        var retryLink = create("a");
+                        retryLink.textContent = "Försök igen";
+                        retryLink.className = "submit-quiz-form";
+                        retryLink.href = "";
+
+                        var submitButton = byId("submit-quiz");
+                        submitButton.parentNode.replaceChild(retryLink, submitButton);
+                    }
+                })
+                .catch(function(e, url) {
+                    // TODO: better error handling
+                    console.log(e, url);
+                });
+        });
+
+        return true;
+    }
+
+    // Create the button for showing/hiding the spoiler text
+    function createShowSpoilerButton(spoiler) {
+        var button = create("button");
+        button.type = "button";
+        button.className = "show-spoiler";
+        button.textContent = "Visa svar";
+
+        // Toggle visibility of the spoiler with classes
+        button.addEventListener("click", function() {
+            if (this.className == "show-spoiler") {
+                // Show the spoiler text
+                spoiler.className = "spoiler show";
+                this.className = "hide-spoiler";
+                this.textContent = "Dölj svar";
+            } else {
+                // Hide the spoiler text
+                spoiler.className = "spoiler";
+                this.className = "show-spoiler";
+                this.textContent = "Visa svar";
+            }
+        });
+
+        return button;
+    }
+
+    // Spoilers
+    function setupSpoilers() {
+        var spoilers = bySelector(".spoiler");
+
+        // Don't do anything if there are no spoilers
+        if (!spoilers) {
+            return false;
+        }
+
+        spoilers.forEach(function(spoiler) {
+            var button = createShowSpoilerButton(spoiler);
+            // TODO: better solution for this (icase nextSibling is undefined)?
+            spoiler.parentNode.insertBefore(button, spoiler.nextSibling);
+        });
+        
+        return true;
+    }
+
     // On page load
     window.addEventListener("load", function load() {
-        // Only run event listener once
+        // Only run the event listener once
         window.removeEventListener("load", load, false);
 
         setContentHeight();
@@ -368,6 +665,10 @@
         addAnchorsToHeaders();
         setTargetForExternalLinks();
         extendCodeExamples();
+        setupSpoilers();
+
+        // TODO: Quiz should not be shown or working if no user is active?
+        setupQuiz();
 
         // Only add web app navigation if we're in standalone mode
         if (inStandalone()) {
