dbb6fab4b468563e174825e90b5f2f650efe59aa
Sebastian Bengtegård <sebbebook@gmail.com>
2015-08-23 22:16:56 +0200

    js stub for the profile page

---
+++ assets/js/profile.js
@@ -0,0 +1,88 @@
+// Don't clutter global namespace
+(function(window, document) {
+    // Strict mode
+    "use strict";
+
+    // Dependencies
+    var React = window.React,
+        Http = window.qwest,
+        Storage = window.localStorage,
+        Cache;
+
+    var Endpoints = {
+        data: "/data.json"
+        // Production
+        // LOCAL
+    };
+
+    // TODO: check if localStorage exists
+    // Setup cache
+    if (Storage.getItem("_mah-looc-data")) {
+        Cache = JSON.parse(Storage.getItem("_mah-looc-data"));
+    } else {
+        Cache = { user: null, domains: null, quizAnswers: null, lastUpdated: null };
+    }
+
+    function updateStorage() {
+        Storage.setItem("_mah-looc-data", JSON.stringify(Cache));
+    }
+
+    // DEBUG
+    if (Cache.user) console.log("User object:", Cache.user);
+    if (Cache.domains) console.log("Domains object:", Cache.domains);
+
+    // Fetch all domains from the server and cache them in localStorage
+    function fetchDomains(cb) {
+        if (Cache.domains) {
+            return cb(Cache.domains);
+        }
+
+        // TODO: how long should the data be cached?
+        return Http.get(Endpoints.data)
+            .then(function(res) {
+                // Build up dependency tree of domains -> modules and then sort them by id
+                var domains = res.domains.map(function(d) {
+                    d.modules = res.modules.filter(function(m) {
+                        return m.domain == d.id;
+                    })
+                    .map(function(m) {
+                        m.mid = m.id;
+                        delete m.id;
+                        return m;
+                    });
+
+                    return d;
+                })
+                .sort(function(a, b) {
+                    return +a.id.substring(1) >= +b.id.substring(1) ? 1 : -1;
+                });
+
+                Cache.domains = domains;
+                updateStorage();
+                cb(domains);
+            })
+            .catch(function(e, url) {
+                // TODO: better error ha
+                console.log(e, url);
+            });
+    }
+
+    var Profile = React.createClass({displayName: "Profile",
+        render: function() {
+            return React.createElement("p", null, "profil");
+        }
+    });
+
+    // On page load
+    window.addEventListener("load", function load() {
+        // Only run event listener once
+        window.removeEventListener("load", load, false);
+        // Render main component to DOM
+        React.render(
+            React.createElement(Profile, null),
+            document.getElementById("profile-container")
+        );
+
+    }, false);
+// Invoke anonymous function
+})(window, document);
